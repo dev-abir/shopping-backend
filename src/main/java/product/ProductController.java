71706a7d8df4f8004cc9f971d3b2466ca910f47f
dev-moss <gangulyabir777@gmail.com>
2020-03-21 17:44:00 +0530

    added a new API entry point to get an array of products based on "quantity" parameter

--- src/main/java/product/ProductController.java
+++ src/main/java/product/ProductController.java
@@ -4,20 +4,43 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
+
 import utils.JsonUtil;
+import utils.Error;
 
 public class ProductController {
 
 	public static final int numberOfProductsForEachCall = 9;
+	public static final int MAX_PRODUCTS_FOR_EACH_CALL = 100;
 
 	public ProductController(final ProductService productService) {
 
 		Spark.get("api/getMoreProducts", new Route() {
-
 			@Override
 			public Object handle(Request request, Response response) {
 				response.type("application/json");
 				return ProductService.getMoreProducts(numberOfProductsForEachCall);
+			}
+		}, JsonUtil::toJson);
+		
+		
+		Spark.get("api/getMoreProducts/:quantity", new Route() {
+			@Override
+			public Object handle(Request request, Response response) {
+				response.type("application/json");
+				int quantity = 0;
+				try {
+					quantity = Integer.parseInt(request.params(":quantity"));
+				} catch (NumberFormatException e) {
+					return new Error("Quantity parameter must be a positive integer within " + MAX_PRODUCTS_FOR_EACH_CALL);
+				}
+				if (quantity < 0) {
+					return new Error("Invalid quantity! It must be a positive integer within " + MAX_PRODUCTS_FOR_EACH_CALL);
+				} else if (quantity > MAX_PRODUCTS_FOR_EACH_CALL) {
+					return new Error("Quantity parameter must be a positive integer within " + MAX_PRODUCTS_FOR_EACH_CALL);
+				} else {
+					return ProductService.getMoreProducts(quantity);
+				}
 			}
 		}, JsonUtil::toJson);
 	}
---
+++ src/main/java/utils/Error.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Error {
+	private String errorMessage;
+
+	public Error(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+}
